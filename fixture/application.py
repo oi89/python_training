a52653d63aa1175e8a62539246dc52dd493d360f
Oleg Ivanov <olegivanov1989@gmail.com>
2023-05-02 15:41:35 +0300

    Set browser and base url as parameters for test run

--- fixture/application.py
+++ fixture/application.py
@@ -6,8 +6,15 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == 'firefox':
+            self.wd = webdriver.Firefox()
+        elif browser == 'chrome':
+            self.wd = webdriver.Chrome()
+        else:
+            raise ValueError(f"Unrecognized browser {browser}")
+
+        self.base_url = base_url
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
@@ -22,7 +29,7 @@ class Application:
             return False
 
     def open_home_page(self):
-        self.wd.get("http://localhost/addressbook/")
+        self.wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
